7946c0efbee7678087ce24495324d17eab98d1a2
elviss116 <eamiranda.d@gmail.com>
2020-02-03 19:17:40 -0500

    SearchView ADd

--- app/src/main/java/com/example/mvvm2020/listRecycler_mvvm/ListRecyclerActivity.java
+++ app/src/main/java/com/example/mvvm2020/listRecycler_mvvm/ListRecyclerActivity.java
@@ -7,15 +7,21 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.SearchView;
 
 import com.example.mvvm2020.R;
 import com.example.mvvm2020.listRecycler_mvvm.adapter.Adapter;
 import com.example.mvvm2020.listRecycler_mvvm.model.UsuarioListResponse;
+import com.example.mvvm2020.listRecycler_mvvm.model.UsuarioOb;
 import com.example.mvvm2020.listRecycler_mvvm.viewmodel.UsuarioListViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListRecyclerActivity extends AppCompatActivity {
 
     RecyclerView rv ;
+    SearchView sv ;
     UsuarioListViewModel usuarioListViewModel;
     final Adapter adapter = new Adapter();
     @Override
@@ -26,9 +32,24 @@ public class ListRecyclerActivity extends AppCompatActivity {
         usuarioListViewModel.getData();
         configView();
         getList();
+
+        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                adapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
     }
     private void configView(){
         rv = findViewById(R.id.rv);
+        sv = findViewById(R.id.svFilter);
         rv.setLayoutManager(new LinearLayoutManager(this));
         rv.setAdapter(adapter);
     }
@@ -41,4 +62,6 @@ public class ListRecyclerActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
--- app/src/main/java/com/example/mvvm2020/listRecycler_mvvm/adapter/Adapter.java
+++ app/src/main/java/com/example/mvvm2020/listRecycler_mvvm/adapter/Adapter.java
@@ -3,6 +3,8 @@ package com.example.mvvm2020.listRecycler_mvvm.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,13 +12,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mvvm2020.R;
 import com.example.mvvm2020.listRecycler_mvvm.model.UsuarioOb;
+import com.example.mvvm2020.save_mvvm.object.Usuario;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> implements Filterable {
 
     private List<UsuarioOb> listaUsuario = new ArrayList<>();
+    private List<UsuarioOb> listaUsuarioFilter = new ArrayList<>();
 
     @NonNull
     @Override
@@ -26,20 +30,57 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.idPk.setText(listaUsuario.get(position).getIdpk());
-        holder.id.setText(listaUsuario.get(position).getId());
-        holder.password.setText(listaUsuario.get(position).getPass());
+        holder.idPk.setText(listaUsuarioFilter.get(position).getIdpk());
+        holder.id.setText(listaUsuarioFilter.get(position).getId());
+        holder.password.setText(listaUsuarioFilter.get(position).getPass());
     }
 
     @Override
     public int getItemCount() {
-        return listaUsuario.size();
+        if (listaUsuarioFilter==null){
+            return 0;
+        }
+        return listaUsuarioFilter.size();
     }
 
     public void setList(List<UsuarioOb> listaUsuario){
         this.listaUsuario=listaUsuario;
+        //this.listaUsuarioFilter = new ArrayList<>(listaUsuario);
+        this.listaUsuarioFilter.addAll(listaUsuario);
         notifyDataSetChanged();
     }
+
+    @Override
+    public Filter getFilter() {
+        return filter;
+    }
+
+    Filter filter=new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+            String charString = charSequence.toString();
+            List<UsuarioOb> filterUsuarioObs = new ArrayList<>();
+            if (charString.isEmpty()){
+                filterUsuarioObs = listaUsuario;
+            }else{
+                for (UsuarioOb row : listaUsuario){
+                    if (row.getId().toLowerCase().contains(charString.toLowerCase())){
+                        filterUsuarioObs.add(row);
+                    }
+                }
+            }
+            FilterResults filterResults = new FilterResults();
+            filterResults.values = filterUsuarioObs;
+
+            return filterResults;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            listaUsuarioFilter = (ArrayList<UsuarioOb>) filterResults.values;
+            notifyDataSetChanged();
+        }
+    };
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
@@ -53,4 +94,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
             password = itemView.findViewById(R.id.model_lista2_passTxt);
         }
     }
+
+
+
 }
